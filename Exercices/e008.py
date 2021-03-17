58abcc9999079559efbde7826dd7632401ebcc0f
Thamires Bernardes <thamires.bernardes@gmail.com>
2021-03-17 15:09:03 -0300

    Fourth

--- Exercices/e008.py
+++ Exercices/e008.py
@@ -1,3 +1,6 @@
+from random import randint
+
+
 def saudacao(msg, nome):
     print(msg, nome)
 
@@ -25,12 +28,14 @@ print(valor)
 
 def verifica(n1):
     if n1 % 3 == 0 and n1 % 5 == 0:
-        return "FizzBuzz"
+        return f'FizzBuzz: {n1} é divisível por 3 e por 5'
     if n1 % 3 == 0:
-        return "Fizz"
+        return f'Fizz: : {n1} é divisível por 3'
     if n1 % 5 == 0:
-        return "Buzz"
-    return n1
+        return f'Buzz: {n1} é divisível por 5'
+    return f'{n1} não é divísivel por 3 nem por 5'
 
 
-print(verifica(30))
+for i in range(100):
+    aleatorio = randint(0, 100)
+    print(verifica(aleatorio))

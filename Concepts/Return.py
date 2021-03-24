c3081cb9e08ff1001ea17c034bf00703543d248b
Thamires Bernardes <60714498+thamiresbernardes@users.noreply.github.com>
2021-03-23 22:15:20 -0300

    Delete Concepts directory

--- Concepts/Return.py
+++
@@ -1,8 +0,0 @@
-def liquidificador(ingrediente1, ingrediente2, ingrediente3):
-    mistura = ingrediente1 + ingrediente2 + ingrediente3
-    bolo = mistura + '=assada'
-    return bolo
-
-
-o_retorno_é_um_bolo = liquidificador('Trigo', 'Açucar', 'Fermento')
-print(o_retorno_é_um_bolo)  # TrigoAçucarFermento=assada

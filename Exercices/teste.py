89927e954cc3df56baafe71fac0879f8e1fb51a5
Thamires Bernardes <thamires.bernardes@gmail.com>
2021-05-27 14:08:55 -0300

    git pull

--- Exercices/teste.py
+++ Exercices/teste.py
@@ -1,1 +1,11 @@
-import nltk.parse.nonprojectivedependencyparser
+def fatorar():
+    n = int(input("Digite o valor de n: "))
+    fat = 1
+    i = 2
+    while i <= n:
+        fat = fat*i
+        i = i + 1
+
+
+print(fat)
+fatorar()

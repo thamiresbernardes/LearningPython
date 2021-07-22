3081378a19f24431f558d00680ac66f7613679a3
Thamires Bernardes <thamires.bernardes@gmail.com>
2021-07-21 21:12:44 -0300

    evo

--- POO/Triangules.py
+++ POO/Triangules.py
@@ -4,12 +4,13 @@ class Poligono:
         self.lados = [0 for i in range(n_lados)]
 
     def le_lados(self):
-        self.lados = [float(input("Digite o tamanho do lado" + str(i+1)))
-                      for i in range(self.n)]
+        self.lados = [
+            float(input("Digite o tamanho do lado" + str(i + 1))) for i in range(self.n)
+        ]
 
     def mostra_lados(self):
         for i in range(self.n):
-            print("Lado", i+1, "tem comprimento", self.lados[i])
+            print("Lado", i + 1, "tem comprimento", self.lados[i])
 
 
 class Triangulo(Poligono):
@@ -18,11 +19,11 @@ class Triangulo(Poligono):
 
     def area(self):
         a, b, c = self.lados
-        s = (a+b+c)/2
-        area = (s*(s-a)*(s-b)((s-c))) ** 0.5
+        s = (a + b + c) / 2
+        area = (s * (s - a) * (s - b)((s - c))) ** 0.5
         print("A área do triângulo é %0.2f" % area)
 
 
-pent = Poligono(5)
+pent = Poligono(6)
 pent.le_lados()
 pent.mostra_lados()

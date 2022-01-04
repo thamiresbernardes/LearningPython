32e18df727148bd1e6611ba6c6328990873c4a71
Thamires Bernardes <thamires.bernardes@gmail.com>
2022-01-04 18:01:58 -0300

    New

---
+++ Exercices/2_condicionaisIf_1.py
@@ -0,0 +1,19 @@
+# Condicionais
+# Operadores relacionais
+# == igualdade
+# != Diferença
+
+# SIMULA EMPRESTIMO
+
+
+def emprestimo(nome, renda, anonascimento, anoatual=2021, renda_m=1000):
+    if (anoatual - anonascimento) >= 18 and (renda >= renda_m):
+        print('Olá {}, você pode pegar empréstimo.'.format(nome))
+    else:
+        print('Olá {}, você NÃO pode pegar empréstimo.'.format(nome))
+
+
+renda_p1 = float(input("Entre com a renda mensal: "))
+idade_p1 = int(input("Entre com seu ano de nasicmento: "))
+nome_p1 = input("Entre com seu nome: ")
+situacao_emprestimo = emprestimo(nome_p1, renda_p1, idade_p1)
--- Exercices/calcIMC.py
+++ Exercices/calcIMC.py
@@ -1,7 +1,5 @@
 class IMC:
-    anoatual = 2021
-
-
+   
 def calculo(peso, altura, ano_nascimento, anoatual=2021):
     peso = peso
     altura = altura
@@ -12,17 +10,17 @@ def calculo(peso, altura, ano_nascimento, anoatual=2021):
 
 
 u1 = calculo(61.8, 1.67, 1989)
-"""
-print('Exercício teste')
-nome = str(input("Qual seu nome?"))
-idade = int(input('Qual sua idade:'))
-altura = float(input('Qual sua altura?'))
-peso = float(input('E seu peso?'))
-anoa = int(input('Entre com o ano atual'))
-nasc = anoa - idade
-imc = peso / pow(altura, 2)
-print(
-    'Olá {}! Seu ano de nascimento é {}, logo você já tem incríveis {} anos vividos. '
-    '\nSua altura é {} e seu peso {}, com essas informações calculamos o seu imc, que é {:.2f}.'
-    .format(nome, nasc, idade, altura, peso, imc))
-"""
+
+
+# print('Exercício teste')
+# nome = str(input("Qual seu nome?"))
+# idade = int(input('Qual sua idade:'))
+# altura = float(input('Qual sua altura?'))
+# peso = float(input('E seu peso?'))
+# anoa = int(input('Entre com o ano atual'))
+# nasc = anoa - idade
+# imc = peso / pow(altura, 2)
+# print(
+#     'Olá {}! Seu ano de nascimento é {}, logo você já tem incríveis {} anos vividos. '
+#     '\nSua altura é {} e seu peso {}, com essas informações calculamos o seu imc, que é {:.2f}.'
+#     .format(nome, nasc, idade, altura, peso, imc))
